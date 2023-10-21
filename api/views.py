874bb8db3cff6ac40dc7daad5d1b2b793fb06fbf
Oleksii Budzinskyi <alexbud1@Oleksiis-MacBook-Air.local>
2023-10-21 18:01:42 +0200

    heroku deployment

--- api/views.py
+++ api/views.py
@@ -183,6 +183,7 @@ class MessageViewSet(viewsets.ViewSet):
             return Response("message is a required field",status = status.HTTP_400_BAD_REQUEST)
         
         # TO DO -> call ml_model.py here
+        chatbot = ChatbotWithHistory()
         answer = chatbot.get_response(request.data['message'])
         return Response(data={'message': answer}, status=status.HTTP_200_OK)
 
